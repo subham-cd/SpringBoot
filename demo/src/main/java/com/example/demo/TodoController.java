11a1fc314fed4106eab6834b290c3219692438e6
Subham Singh <subham0506singh@gmail.com>
2026-03-21 17:02:56 +0530

    Implement get and post method for todos

--- demo/src/main/java/com/example/demo/TodoController.java
+++ demo/src/main/java/com/example/demo/TodoController.java
@@ -3,19 +3,33 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
 
-    private static List<Todo> todos ;
+    private static List<Todo> todolist ;
 
     public TodoController(){
-        todos = new ArrayList<>();
-        todos.add(new Todo(1,1,"Todo1",false));
-        todos.add(new Todo(2,2,"Todo2",true));
+        todolist = new ArrayList<>();
+        todolist.add(new Todo(1,1,"Todo1",false));
+        todolist.add(new Todo(2,2,"Todo2",true));
     }
-    
+
+
+      @GetMapping("/todos")
+    public List<Todo> getTodos(){
+        return todolist;
+    }
+    @PostMapping("/todos")
+    public Todo createTodo(@RequestBody Todo newTodo){
+          todolist.add(newTodo);
+          return newTodo;
+    }
+
 
     
 }
