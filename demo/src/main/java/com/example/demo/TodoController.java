5a0eaad1a8cb83adea5ea42c1eeb0fda0f6c6c36
Subham Singh <subham0506singh@gmail.com>
2026-03-22 12:18:19 +0530

    Learned more api and controllers

--- demo/src/main/java/com/example/demo/TodoController.java
+++ demo/src/main/java/com/example/demo/TodoController.java
@@ -3,10 +3,17 @@ package com.example.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
 
 @RestController
 public class TodoController {
@@ -21,15 +28,31 @@ public class TodoController {
 
 
       @GetMapping("/todos")
-    public List<Todo> getTodos(){
-        return todolist;
+    public ResponseEntity<List<Todo>> getTodos(){
+        // return ResponseEntity.ok(todolist);
+        return ResponseEntity.status(HttpStatus.OK).body(todolist);
     }
     @PostMapping("/todos")
-    public Todo createTodo(@RequestBody Todo newTodo){
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Todo> createTodo(@RequestBody Todo newTodo){
           todolist.add(newTodo);
-          return newTodo;
+        //   return newTodo;
+        return ResponseEntity.status(HttpStatus.CREATED).body(newTodo);
+       
+    }
+    @GetMapping("/todos/{todoId}")
+    // public ResponseEntity<Todo> getTodoById(@PathVariable long todoId){
+     public ResponseEntity<?> getTodoById(@PathVariable long todoId){
+        for(Todo todo:todolist){
+            if(todo.getId()==todoId){
+              return ResponseEntity.ok(todo);
+
+            }
+        }
+        // return ResponseEntity.notFound().build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Todo not found");
     }
 
 
-    
+     
 }
